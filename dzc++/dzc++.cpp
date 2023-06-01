8e754ddbd65d0343b3282faae7aad02b40366019
Admin <Admin@WIN-5I7J1UL4AE7>
2023-06-01 21:18:00 +0300

    Добавьте файлы проекта.

---
+++ dzc++/dzc++.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+	cout << "      .   "<<endl;
+	cout << "   .    ."<<endl;
+	cout << " .    .    ."<<endl;
+	cout << ".   .    .   .";
+
+
+
+		
+}
+
+  
